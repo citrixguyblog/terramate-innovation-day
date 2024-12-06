33bdcb1d4ccd37d29277ba51252444bcf742996e
Stefan Rapp <srapp@xpirit.com>
2024-12-06 09:44:29 +0100

    Co-authored-by: Julian Mooren <citrixguyblog@users.noreply.github.com>

--- deploy/base/provider.tf
+++ deploy/base/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 1.7.4"
 
-  backend "azurerm" {
-  }
+  #  backend "azurerm" {
+  #  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
